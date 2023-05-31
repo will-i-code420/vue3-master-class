0d4ef716190579cd143d6f1eebe248e2e7401864
will-i-code420 <waronow123@gmail.com>
2023-05-30 20:59:08 -0700

    added eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,16 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  rules: {
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
