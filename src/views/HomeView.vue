a933ca65e7dd0d0260f4bf5f9278a9e56af2d93c
will-i-code420 <waronow123@gmail.com>
2023-06-02 15:20:16 -0700

    removed starter files/setup homeview to use data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { reactive } from 'vue'
+import sourceData from '@/data.json'
+const threads = reactive(sourceData.threads)
+const posts = reactive(sourceData.posts)
+const users = reactive(sourceData.users)
+function getPost(id) {
+  return posts.find((post) => post.id === id)
+}
+function getUser(id) {
+  const post = getPost(id)
+  return users.find((user) => user.id === post.userId)
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div v-for="thread in threads" :key="thread.id">
+      <h2>{{ thread.title }}</h2>
+      <div v-for="postId in thread.posts" :key="postId">
+        <p>{{ getUser(postId).name }}</p>
+        <p>{{ getPost(postId).text }}</p>
+      </div>
+    </div>
   </main>
 </template>
