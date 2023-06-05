3496626d03f86e5c9171b0ba5d2b0c54cecc1582
will-i-code420 <waronow123@gmail.com>
2023-06-05 15:30:46 -0700

    cleaned up homeview code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,5 @@
 <script setup>
-import { reactive } from 'vue'
 import ThreadList from '../components/ThreadList.vue'
-import sourceData from '@/data.json'
-const threads = reactive(sourceData.threads)
-const posts = reactive(sourceData.posts)
-const users = reactive(sourceData.users)
-function getPost(id) {
-  return posts.find((post) => post.id === id)
-}
-function getUser(id) {
-  const post = getPost(id)
-  return users.find((user) => user.id === post.userId)
-}
 </script>
 
 <template>
