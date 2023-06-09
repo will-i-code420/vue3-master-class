c4af44e2c3909932409a99d9016b9693a2fbdeb5
will-i-code420 <waronow123@gmail.com>
2023-06-08 21:12:05 -0700

    created user-profile-card

---
+++ src/components/UserProfileCard.vue
@@ -0,0 +1,46 @@
+<script setup>
+defineProps({
+  user: {
+    type: Object,
+    required: true
+  },
+  userPostCount: {
+    type: Number,
+    required: true
+  },
+  userThreadCount: {
+    type: Number,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="profile-card">
+    <p class="text-center">
+      <img :src="user.avatar" :alt="`${user.name} profile pic`" class="avatar-xlarge" />
+    </p>
+
+    <h1 class="title">{{ user.username }}</h1>
+
+    <p class="text-lead">{{ user.name }}</p>
+
+    <p class="text-justify">{{ user.bio || 'No bio specified' }}</p>
+
+    <span class="online">{{ user.username }} is online</span>
+
+    <div class="stats">
+      <span>{{ userPostCount }} posts</span>
+      <span>{{ userThreadCount }} threads</span>
+    </div>
+
+    <hr />
+
+    <p v-if="user.website" class="text-large text-center">
+      <i class="fa fa-globe"></i>
+      <router-link :to="user.website"> Visit {{ user.username }}</router-link>
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { computed } from 'vue'
 import PostList from '@/components/Postlist.vue'
+import UserProfileCard from '@/components/UserProfileCard.vue'
 import { useUsersStore } from '@/stores/users'
 import { usePostsStore } from '@/stores/posts'
 import { useThreadsStore } from '@/stores/threads'
@@ -12,31 +13,11 @@ const userThreads = computed(() => useThreadsStore().getThreads('user', user.val
 <template>
   <div class="flex-grid">
     <div class="col-3 mt-2">
-      <div class="profile-card">
-        <p class="text-center">
-          <img :src="user.avatar" :alt="`${user.name} profile pic`" class="avatar-xlarge" />
-        </p>
-
-        <h1 class="title">{{ user.username }}</h1>
-
-        <p class="text-lead">{{ user.name }}</p>
-
-        <p class="text-justify">{{ user.bio || 'No bio specified' }}</p>
-
-        <span class="online">{{ user.username }} is online</span>
-
-        <div class="stats">
-          <span>{{ userPosts.length }} posts</span>
-          <span>{{ userThreads.length }} threads</span>
-        </div>
-
-        <hr />
-
-        <p v-if="user.website" class="text-large text-center">
-          <i class="fa fa-globe"></i>
-          <router-link :to="user.website"> Visit {{ user.username }}</router-link>
-        </p>
-      </div>
+      <UserProfileCard
+        :user="user"
+        :userPostCount="userPosts.length"
+        :userThreadCount="userThreads.length"
+      />
       <!-- 
     TODO: Implement Member Since and Edit Profile
       <p class="text-xsmall text-faded text-center">
