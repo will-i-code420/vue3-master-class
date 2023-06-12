028865c2b3d68894e7a2db91bedd15f304292573
will-i-code420 <waronow123@gmail.com>
2023-06-11 21:09:09 -0700

    installed and initialized firebase sdk

--- src/main.js
+++ src/main.js
@@ -6,11 +6,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import BaseDateDisplay from '@/components/BaseDateDisplay.vue'
 import router from './router'
+import { initializeApp } from 'firebase/app'
+import firebaseConfig from '@/config/firebase'
+
+const fbApp = initializeApp(firebaseConfig)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(fbApp)
 
 app.component('BaseDateDisplay', BaseDateDisplay)
 
