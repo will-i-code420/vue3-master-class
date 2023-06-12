6e7233a2ef6ee52ff496d500cecd0393eb48ce8b
will-i-code420 <waronow123@gmail.com>
2023-06-11 22:25:51 -0700

    added some todo notes for comments to lessons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,3 +13,7 @@ const { categories } = storeToRefs(useCategoriesStore())
 </template>
 
 <style scoped></style>
+
+TODO:Leave notes on lessons 65: had to add types: module to package.json and may be due to using
+vite and not cli, also check about import.meta.env TODO: leave note on lessons 66: new lifecycle
+chart shows setup compostion above all other life cycles, so why use anything else
